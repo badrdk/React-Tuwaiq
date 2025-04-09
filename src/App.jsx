ee552195a5422cb83a950cc63849c122aea22618
badr aldodokhayel <badrdk0@gmail.com>
2025-04-09 19:43:53 +0300

    test for github

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
     <div>
         <h1>every element must have one parents </h1>
         <span>10+10 = {10+10}</span>
+        <h1>git test</h1>
         <p>any element tag must be closed </p>
+        <h1>test test test git </h1>
         <p className="paragraph" > this is s class name</p>
         <span style={{color: 'red' ,  backgroundColor:'white'}}>black</span>
 
